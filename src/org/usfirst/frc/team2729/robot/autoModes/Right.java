8c8f5eedd8883a1009dc29bdc3379b3992411bd4
SimarKareer <skareer@outlook.com>
2017-02-04 11:12:28 -0500

    basic centeralign

--- src/org/usfirst/frc/team2729/robot/autoModes/Right.java
+++ src/org/usfirst/frc/team2729/robot/autoModes/Right.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team2729.robot.autoModes;
 
 import org.usfirst.frc.team2729.robot.Robot;
+import org.usfirst.frc.team2729.robot.commands.CenterTurn;
+import org.usfirst.frc.team2729.robot.commands.VisionAlignment;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
@@ -9,39 +11,8 @@ public class Right extends CommandGroup{
 	
 
 	public Right() {
-		//addSequential(DriveForwardDistance(drives, turns));
-		alignment();
-	
-		//addSequential()
+		//addSequential(new VisionAlignment());	
+		addSequential(new CenterTurn());
 	}
 	
-	public void alignment() {
-		final int RESOLUTION = 544;
-		NetworkTable table;
-		
-		double leftSpeed = 200;
-		double rightSpeed = 200;
-		
-		table = NetworkTable.getTable("Vision");
-		int defaultValue = 0;
-		while(table.getNumber("est_distance", 0) > 0.5) {
-			int shift = (int) table.getNumber("shift", defaultValue); // 1 = left shifted 0 = right shifted
-			int centerX = (int) table.getNumber("midpoint_x", RESOLUTION/2); //half of x res
-			requires(Robot.driveTrain);
-			
-			//if (LR == 1)
-				//rightSpeed += 
-			
-			if(centerX < (RESOLUTION/2) + 10 && centerX > (RESOLUTION/2) - 10) {
-				leftSpeed = 200;
-				rightSpeed = 200;
-			}
-			else if(centerX > RESOLUTION/2 && leftSpeed < 300)
-				leftSpeed+=10;
-			else if(centerX < RESOLUTION/2 && rightSpeed < 300)
-				rightSpeed +=10;
-			
-			Robot.driveTrain.tankDrive(leftSpeed, rightSpeed); //1500 max
-		}
-	}
 }
--- src/org/usfirst/frc/team2729/robot/commands/CenterTurn.java
+++ src/org/usfirst/frc/team2729/robot/commands/CenterTurn.java
@@ -5,8 +5,8 @@ import org.usfirst.frc.team2729.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
-public class VisionAlignment extends Command{
-	public VisionAlignment() {
+public class CenterTurn extends Command{
+	public CenterTurn() {
 	}
 	
 	@Override
@@ -18,29 +18,26 @@ public class VisionAlignment extends Command{
 	protected void execute() {
 		final int RESOLUTION = 544;
 		NetworkTable table;
-		
-		double leftSpeed = 200;
-		double rightSpeed = 200;
-		
 		table = NetworkTable.getTable("Vision");
-		int defaultValue = 0;
-		while(table.getNumber("est_distance", 0) > 0.5) {
-			int shift = (int) table.getNumber("shift", defaultValue); // 1 = left shifted 0 = right shifted
+		int leftSpeed = 0;
+		int rightSpeed = 0;
+		while(true) {
+			int shift = (int) table.getNumber("shift", 0); // (+) = left shifted (-) = right shifted
 			int centerX = (int) table.getNumber("midpoint_x", RESOLUTION/2); //half of x res
 			requires(Robot.driveTrain);
 			
-			//if (LR == 1)
-				//rightSpeed += 
-			
 			if(centerX < (RESOLUTION/2) + 10 && centerX > (RESOLUTION/2) - 10) {
-				leftSpeed = 200;
-				rightSpeed = 200;
+				leftSpeed = 0;
+				rightSpeed = 0;
 			}
-			else if(centerX > RESOLUTION/2 && leftSpeed < 300)
+			else if(centerX > RESOLUTION/2 && leftSpeed < 300) {
 				leftSpeed+=10;
-			else if(centerX < RESOLUTION/2 && rightSpeed < 300)
+				rightSpeed -=10;
+			}
+			else if(centerX < RESOLUTION/2 && rightSpeed < 300) {
 				rightSpeed +=10;
-			
+				leftSpeed -=10;
+			}
 			Robot.driveTrain.tankDrive(leftSpeed, rightSpeed); //1500 max
 		}
 	}
